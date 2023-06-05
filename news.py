58c3b20ddfeb5b3ce1bbf8b1a0e93ef8891ae974
hjh0207 a <h2j10207@gmail.com>
2023-06-05 02:21:51 +0000

    ㅜㅜㅜ

--- news.py
+++ news.py
@@ -3,11 +3,11 @@ from bs4 import BeautifulSoup
 
 url = "https://news.naver.com/main/list.naver?mode=LSD&mid=sec&sid1=001"
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-response = requests.get(url)
-
+response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
-    for tr in soup.select("#wrap > table > tbody > tr"):
-        title = tr.select_one("#main_content > div.list_body.newsflash_body > ul.type06_headline > li:nth-child(1) > dl > dt:nth-child(2) > a")
-        print(title.text.strip())
+    for title in soup.select(".type06_headline a"):
+        print(title.get_text(strip=True))
+else:
+    print(f"HTTP 요청 실패 코드: {response.status_code}")
